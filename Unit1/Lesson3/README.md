5c17d164ca414017b1291e5f2de536c25954496c
Kathan Vakharia <kathankv5201@gmail.com>
2020-08-17 18:14:22 +0530

    Added lesson 3

---
+++ Unit1/Lesson3/README.md
@@ -0,0 +1,6 @@
+# How to execute this file
+
+1. First Build it : `go build main.go`
+2. Then run it by passing your guess as command line argument : `go run ./main.exe 55`
+
+Note : Executables of mac and linux won't have .exe extension
---
+++ Unit1/Lesson3/main.go
@@ -0,0 +1,39 @@
+//* Static Guessing Game
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+var userGuess,_ = strconv.Atoi(os.Args[1])
+
+func main() {
+	fmt.Println("Welcome to the Guessing Game!")
+	fmt.Println("##############################")
+	var steps = 0
+	rand.Seed(time.Now().UnixNano())
+	//- Execute till generated random number is not equal to guess
+	for {
+		computerGuess := rand.Intn(100) + 1
+		if computerGuess < userGuess {
+			fmt.Println("Computers Guess", computerGuess)
+			fmt.Println("Too Low!")
+		} else if computerGuess > userGuess {
+			fmt.Println("Computers Guess", computerGuess)
+			fmt.Println("Too High!")
+		} else if computerGuess == userGuess {
+			//When guess matches
+			fmt.Println("Ahan!")
+			fmt.Printf("Computer's Guess: %v ,\nMyGuess: %v\n", computerGuess, userGuess)
+			break
+		}
+		time.Sleep(time.Millisecond*500)
+		steps++
+	}
+	fmt.Printf("The computer guessed in %v tries", steps)
+
+}
